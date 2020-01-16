2305ac91347d9f8585b32297fa89b739affbaba8
FloNumbers <floriansole@gmail.com>
2020-01-16 14:09:14 +0100

    about me page styled

--- index.php
+++ index.php
@@ -29,30 +29,51 @@
     </div>
     <div id="Home" class="landingtext">
         <h1>I'm Florian Solé</h1>
-        <h2>Developer</h2>
+        <h2>Junior developer</h2>
         <ul>
             <li><a class="fab fa-linkedin-in" href="https://www.linkedin.com/in/florian-sol%C3%A9-76a6b6144/" target="_blank"></a></li>
             <li><a class="fab fa-github" href="https://github.com/FloNumbers?tab=repositories" target="_blank"></a></li>
         </ul>
     </div>
     <div class="solid">
-        <div id="About" class="aboutPage">
-            <h1>About Me</h1>
-            <p>I am Florian Solé. I was born in Tournai in Belgium, but i moved to the Netherlands when i was around 1
-                year old and still live there to this day.</p> <br>
-            <h4>School</h4>
-            <p>I am currently studying Application Developer at Scalda in Vlissingen. Before this I studied HBO-ICT at
-                the HZ university, but I didn't meet the
-                requirement to pass on to the next year. Despite all that, I'd say that I have found my passion in
-                programming and am surely planning on continuing this.</p> <br>
-            <h4>Hobbies</h4>
-            <p>In my free time you'll probably find me behind my computer playing some games. That does not mean I don't
-                do sports though. I am the goalkeeper for my
-                local waterpolo team, and I absolutely love to go windsurfing in the summer.</p>
+        <div id="About" class="page">
+            <div class="pageListHeader">
+                <div class="pageHeader">
+                    <h1>About Me</h1>
+                    <p>I am Florian Solé. I was born in Tournai in Belgium, but i moved to the Netherlands when i was around 1
+                        year old and still live there to this day.</p>
+                </div>
+                <div class="aboutImageContainer"><img style="margin-top:2rem;" class="aboutImage" src="assets\Flag_of_Belgium.png" width="100" height="100"></div>
+            </div>
+            <div class="pageList">
+                <div class="pageText"> 
+                    <h4>School</h4>
+                    <p>I am currently studying Application Developer at Scalda in Vlissingen. Before this I studied HBO-ICT at
+                        the HZ university, but I didn't meet the
+                        requirements to pass on to the next year. Despite all that, I'd say that I have found my passion in
+                        programming and am surely planning on continuing this.</p>
+                </div>
+                <div class="aboutImageContainer"><img class="aboutImage" src="assets\logo-Scalda.jpg" width="140" height="100"></div>
+            </div>
+            <div class="pageList">
+                <div class="pageText">
+                    <h4>Hobbies</h4>
+                    <p>In my free time you'll probably find me behind my computer playing some games. That does not mean I don't
+                        do sports though. I am the goalkeeper for my
+                        local waterpolo team, and I absolutely love to go windsurfing in the summer.</p>
+                </div>
+                <div class="aboutImageContainer"><img class="aboutImage" src="assets\windsurfen.jpg" width="100" height="100"></div>
+            </div>
+            <div class="pageList">
+                <h4>Work experience</h4>
+                <ul>
+                    <li><p></p></li>
+                </ul>
+            </div>
         </div>
     </div>
-    <div id="Projects" class="projectsPage">
-        <div class="projectsPageHeader">
+    <div id="Projects" class="page">
+        <div class="pageHeader">
             <h1>Projects</h1>
             <p>Ever since I started programming I have offcourse doen a couple of projects. Some I am defenitely more
                 proud of than others, but I'll list a few here.
@@ -64,8 +85,8 @@
         <ul>
             <li>
                 <h2><a style="color:white" href="https://sleepy-harbor-96591.herokuapp.com/" target="_blank">original portfolio</a></h2>
-                <div class="projectsPageList">
-                    <div class="ppl2Text">
+                <div class="pageList">
+                    <div class="pageText">
                         <p>As the first project I am listing here, you would probably think that this is the project that
                             I'm most proud of. But that is not the case here.
                             This project was my first ever github project. This project was meant as a portfolio assignement
@@ -76,21 +97,21 @@
                             achived with only 1 or 2 months of any coding experience.
                         </p>
                     </div>
-                    <div class="pplImage">
-                        <img src="assets\portfolioOriginal.jpg" width="500" height="300">
+                    <div class="pplImageContainer">
+                        <img class="pplImage" src="assets\portfolioOriginal.jpg" width="500" height="300">
                     </div>
                 </div>
             </li>
             <li style="text-align:right; direction:rtl">
                 <h2><a style="color:white;" href="https://flonumbers.github.io/basisschoolproject/" target="_blank">Bassisschoolproject</a></h2>
-                <div class="projectsPageList2">
-                    <div class="pplImage">
-                    <img src="assets\basisschoolProject.jpg" width="500" height="300">
+                <div class="pageList2">
+                    <div class="pplImageContainer">
+                        <img class="pplImage" src="assets\basisschoolProject.jpg" width="500" height="300">
                     </div>
-                    <div class="pplText">
+                    <div class="pageText">
                         <p>This one is another project I made for an assignement at the HZ university. This was a group
                             project that we had to make for a primary school.
-                            It had to be some kind of small game that had to do with a specific course they teached at
+                            It had to be some kind of small game that had to do with a specific course they taught at
                             that school. We chose history. The whole game is written in typescript
                             and is made in such a way that you never have to reload the page. You can even save your
                             progress if you don't delete your cookies.
@@ -103,16 +124,16 @@
             </li>
             <li>
                 <h2><a style="color:white" href="https://www.ditiseendomein.nl/" target="_blank">This very portfolio</a></h2>
-                <div class="projectsPageList">
-                    <div class="pplText">
+                <div class="pageList">
+                    <div class="pageText">
                         <p>So this very website you're in right now is also a school assignement, but this one is a school
                             assignement for my current school, Scalda.
                             I will be keeping this website up to date with new projects and thing I'm learning, so everyone
                             who's interested can take a peek.
                         </p>
                     </div>
-                    <div class="pplImage">
-                    <img src="assets\portfolioHuidig.jpg" width="500" height="300">
+                    <div class="pplImageContainer">
+                        <img class="pplImage" src="assets\portfolioHuidig.jpg" width="500" height="300">
                     </div>
                 </div>
             </li>
